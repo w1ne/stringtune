40700d624c21219049f9806ca944ee2f6c7db475
Andrii <14119286+w1ne@users.noreply.github.com>
2023-07-18 13:10:53 +0200

    working pwa

--- stringtune/static/sw.js
+++ stringtune/static/sw.js
@@ -1,11 +1,25 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    fetch('index.json')
+    fetch('./index.json')
       .then(response => response.json())
       .then(files =>
         caches.open('stringtune-tuner-cache').then(function(cache) {
           return cache.addAll(files);
         })
       )
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // Not in cache, return from network
+        return fetch(event.request);
+      })
   );
 });
